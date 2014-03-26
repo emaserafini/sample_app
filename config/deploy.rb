43fac706a7ae415ceeeb6c2bb178719391e97b3d
Emanuele Serafini <ema.serafini@gmail.com>
2014-03-26 17:03:44 +0100

    Improve recipes

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,25 +37,8 @@ set :linked_dirs, %w{log tmp}
 
 namespace :deploy do
 
-  after :deploy, "puma:hot_restart"
-
-  # desc 'Restart application'
-  # task :restart do
-  #   on roles(:app), in: :sequence, wait: 5 do
-  #     # Your restart mechanism here, for example:
-  #     # execute :touch, release_path.join('tmp/restart.txt')
-  #   end
-  # end
-  #
-  # after :publishing, :restart
-  #
-  # after :restart, :clear_cache do
-  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
-  #     # Here we can do anything such as:
-  #     # within release_path do
-  #     #   execute :rake, 'cache:clear'
-  #     # end
-  #   end
-  # end
+  # uncomment this line for zero downtime deploy
+  # run after first deploy and "cap staging nginx:setup" and "cap staging puma:setup"
+  # after :deploy, "puma:hot_restart"
 
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,4 +3,13 @@ set :rails_env, 'staging'
 set :user, 'web'
 set :app_env, "#{fetch(:application)}_#{fetch(:rails_env)}"
 
-server 'your.ip', user: "#{fetch(:user)}", roles: %w{web app db}
+# Puma configuration
+set :puma_config,  -> { "#{shared_path}/config/puma.rb" }
+set :puma_pid,     -> { "#{current_path}/tmp/puma.pid" }
+set :puma_sock,    -> { "#{current_path}/tmp/puma.sock" }
+set :puma_state,   -> { "#{shared_path}/tmp/puma.state" }
+set :puma_log,     -> { "#{shared_path}/log/puma.log" }
+set :puma_workers, 0
+set :puma_threads, '0, 16'
+
+server 'ENTER.YOUR.SERVER.IP', user: "#{fetch(:user)}", roles: %w{web app db}
